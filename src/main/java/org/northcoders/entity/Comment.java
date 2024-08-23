d93b28c7d6224b24167740c498efe9b0ae5bc9a8
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-23 12:30:20 +0100

    feat: create Comment class with fields and relationships with author & recipe

--- src/main/java/org/northcoders/entity/Comment.java
+++ src/main/java/org/northcoders/entity/Comment.java
@@ -1,2 +1,24 @@
-package org.northcoders.entity;public class Comment {
+package org.northcoders.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ManyToAny;
+
+import java.time.LocalDate;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue
+    int id;
+
+    String text;
+    LocalDate datePosted;
+
+    @ManyToOne(fetch=LAZY)
+    User author;
+
+    @ManyToOne(fetch = LAZY)
+    Recipe associatedRecipe;
 }
