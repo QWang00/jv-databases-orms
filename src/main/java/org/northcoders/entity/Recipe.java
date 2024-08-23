fc8d88d2c71e41bfbc01cecc0de07f078f35fd09
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-23 12:01:47 +0100

    fix: add entity annotation in User class

---
+++ src/main/java/org/northcoders/entity/Recipe.java
@@ -0,0 +1,2 @@
+package org.northcoders.entity;public class Recipe {
+}
--- src/main/java/org/northcoders/entity/User.java
+++ src/main/java/org/northcoders/entity/User.java
@@ -1,16 +1,21 @@
 package org.northcoders.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDate;
 
+@Entity
 public class User {
     @Id
     @GeneratedValue
     int id;
 
+    @NotNull
     String userName;
+
     String email;
     String password;
     Role role;
