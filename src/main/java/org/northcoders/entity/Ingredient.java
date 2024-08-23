91fb3ba82d4752b1a8f69a628104e74874f3649f
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-23 12:15:01 +0100

    fix: add entity and NotNull annotations for Tag class

--- src/main/java/org/northcoders/entity/Ingredient.java
+++ src/main/java/org/northcoders/entity/Ingredient.java
@@ -1,13 +1,17 @@
 package org.northcoders.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import org.jetbrains.annotations.NotNull;
 
+@Entity
 public class Ingredient {
     @Id
     @GeneratedValue
     int id;
 
+    @NotNull
     String name;
     int quantity;
     String unitOfMeasurement;
--- src/main/java/org/northcoders/entity/Tag.java
+++ src/main/java/org/northcoders/entity/Tag.java
@@ -1,12 +1,16 @@
 package org.northcoders.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import org.jetbrains.annotations.NotNull;
 
+@Entity
 public class Tag {
     @Id
     @GeneratedValue
     int id;
 
+    @NotNull
     String name;
 }
