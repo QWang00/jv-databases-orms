e074b84833c43fd0320857f1f9a8e2a4c809c720
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-23 12:45:38 +0100

    feat: create Image class with fields and ManyToOne with recipe

--- src/main/java/org/northcoders/entity/Image.java
+++ src/main/java/org/northcoders/entity/Image.java
@@ -1,2 +1,24 @@
-package org.northcoders.entity;public class Image {
+package org.northcoders.entity;
+
+import jakarta.persistence.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue
+    int id;
+
+    @NotNull
+    String fileName;
+
+    int fileData;
+
+    @ManyToOne(fetch = LAZY)
+    Recipe associatedRecipe;
+
 }
