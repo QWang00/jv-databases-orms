2011a2f5a7dd0a56e535f927e9e5df316aa7dbd7
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-23 12:51:25 +0100

    feat: create Rating class with fields and relationships with User & Recipe

--- src/main/java/org/northcoders/entity/Rating.java
+++ src/main/java/org/northcoders/entity/Rating.java
@@ -1,2 +1,27 @@
-package org.northcoders.entity;public class Rating {
+package org.northcoders.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import org.jetbrains.annotations.NotNull;
+import static jakarta.persistence.FetchType.LAZY;
+import java.time.LocalDate;
+
+@Entity
+public class Rating {
+    @Id
+    @GeneratedValue
+    int id;
+
+    @NotNull
+    int value;
+
+    LocalDate dateRated;
+
+    @ManyToOne(fetch = LAZY)
+    Recipe associatedRecipe;
+
+    @ManyToOne(fetch = LAZY)
+    User rater;
 }
