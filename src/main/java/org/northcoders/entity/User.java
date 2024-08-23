c81c8b17874fe4716267f1b76f78f5c9fabab123
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-23 11:47:51 +0100

    feat: create User class with fields

--- src/main/java/org/northcoders/entity/User.java
+++ src/main/java/org/northcoders/entity/User.java
@@ -1,2 +1,18 @@
-package org.northcoders.entity;public class User {
+package org.northcoders.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+public class User {
+    @Id
+    @GeneratedValue
+    int id;
+
+    String userName;
+    String email;
+    String password;
+    Role role;
+    LocalDate dateRegistered;
 }
